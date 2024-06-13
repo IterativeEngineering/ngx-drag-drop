6ff25067d161c8f4f22d96af5ced0a38c57ac9a1
Adam Drozdz <adam.drozdz@iterative.pl>
2024-06-13 10:09:27 +0200

    Export default calculateDragImageOffset function

--- projects/dnd/src/public-api.ts
+++ projects/dnd/src/public-api.ts
@@ -6,5 +6,5 @@ export * from './lib/dnd-draggable.directive';
 export * from './lib/dnd-dropzone.directive';
 export * from './lib/dnd-handle.directive';
 export * from './lib/dnd-types';
-export { DndDragImageOffsetFunction } from './lib/dnd-utils';
+export { DndDragImageOffsetFunction, calculateDragImageOffset } from './lib/dnd-utils';
 export * from './lib/dnd.module';
