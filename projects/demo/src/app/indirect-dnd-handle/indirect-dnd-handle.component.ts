828072e99655e29bbeb8b51ef849f78495c806f3
Adam Drozdz <adam.drozdz@iterative.pl>
2026-06-26 09:53:29 +0200

    Remove demo project

--- projects/demo/src/app/indirect-dnd-handle/indirect-dnd-handle.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, HostBinding } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { DndHandleDirective } from 'ngx-drag-drop';
-
-@Component({
-  templateUrl: './indirect-dnd-handle.component.html',
-  styleUrls: ['./indirect-dnd-handle.component.scss'],
-  standalone: true,
-  imports: [MatIconModule, DndHandleDirective],
